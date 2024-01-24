5e392b914f0676c8d578fd42fa54c610758ec33f
jame12945 <jame12945@gmail.com>
2024-01-24 18:06:04 +0700

    fetch data from api backend

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,32 +3,49 @@ import { Table } from 'antd'
 import axios from 'axios'
 
 function TableDynamic() {
-  const [displayText, setDisplayText] = useState()
   const [columns, setColumn] = useState([])
   const [dataSource, setDataSource] = useState([])
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://dummyjson.com/quotes')
-        const list = response.data.quotes || []
-        console.log('list:', list)
-        const firstObject = list[0] || {}
-        console.log('firstObject:', firstObject)
-        const cols = []
-        for (const key in firstObject) {
-          const col = {
-            title: key,
-            dataIndex: key,
-          }
-          cols.push(col)
-          console.log('cols:', cols)
+        const response = await axios.post(
+          'http://localhost:3000/booking-fe/filter',
+          {}
+        )
+        const data = response.data.data.filterDateBooking || []
+
+        if (data.length > 0) {
+          const cols = [
+            {
+              title: 'Type',
+              dataIndex: 'truckType',
+            },
+            {
+              title: 'Appointment Date',
+              dataIndex: 'bookingDate',
+            },
+            {
+              title: 'Car Registration',
+              dataIndex: 'truckLicensePlate',
+            },
+            {
+              title: 'Driver',
+              dataIndex: 'driverName',
+            },
+            {
+              title: 'BookingId',
+              dataIndex: 'id',
+            },
+          ]
+
+          setColumn(cols)
+          setDataSource(data)
         }
-        setColumn(cols)
-        setDataSource(response.data.quotes)
       } catch (error) {
         console.error('fetchingData Error ', error)
       }
     }
+
     fetchData()
   }, [])
 
--- src/pages/LprPage.tsx
+++ src/pages/LprPage.tsx
@@ -4,24 +4,12 @@ import TableDynamic from '../components/Table'
 function LprPage() {
   const [data, setData] = useState([])
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/users/1')
-      .then((res) => setData(res.data))
-  })
-
-  //   console.log(data)
   return (
     <div className=" px-3 py-2  m-auto">
-      {/* <h1>About</h1>
-      <hr />
-      <h3>My Name is {data.name}</h3>
-      <ul>
-        <li>email :{data.email}</li>
-        <li>phone :{data.phone}</li>
-        <li>website :{data.website}</li>
-      </ul> */}
-      <TableDynamic />
+      <p>
+        hello LPR ต้องเพื่มจากเดิมคือต้อง post ข้อมูลลงไปได้
+        แยกอีกตารางจริงๆแล้ว
+      </p>
     </div>
   )
 }
