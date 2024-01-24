58a054683813c2bc4088d67777b0c4610691c19b
jame12945 <jame12945@gmail.com>
2024-01-24 15:37:25 +0700

    update table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,25 +4,26 @@ import axios from 'axios'
 
 function TableDynamic() {
   const [displayText, setDisplayText] = useState()
-  const [columns, setColumn] = useState([
-    {
-      title: 'ID',
-      dataIndex: 'id',
-    },
-    {
-      tilte: 'Quote',
-      dataIndex: 'quote',
-    },
-    {
-      title: 'Author',
-      dataIndex: 'author',
-    },
-  ])
+  const [columns, setColumn] = useState([])
   const [dataSource, setDataSource] = useState([])
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://dummyjson.com/quotes')
+        const list = response.data.quotes || []
+        console.log('list:', list)
+        const firstObject = list[0] || {}
+        console.log('firstObject:', firstObject)
+        const cols = []
+        for (const key in firstObject) {
+          const col = {
+            title: key,
+            dataIndex: key,
+          }
+          cols.push(col)
+          console.log('cols:', cols)
+        }
+        setColumn(cols)
         setDataSource(response.data.quotes)
       } catch (error) {
         console.error('fetchingData Error ', error)
